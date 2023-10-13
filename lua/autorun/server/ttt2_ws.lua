6b860178e09df25ad420246e498da7d7480865e8
techbot <techbot121@gmail.com>
2023-10-13 17:47:01 +0200

    add some new stuff

--- lua/autorun/server/ttt2_ws.lua
+++ lua/autorun/server/ttt2_ws.lua
@@ -1,10 +1,14 @@
 local list = {
-"1357204556",
-"2586573261",
-"105875340",
-"654570222",
-"2730454615",
-
+	"1357204556",	-- TTT2
+	"2586573261",	-- Huh
+	"105875340",	-- Jihad Bomb
+	"654570222",	-- Banana Bomb
+	"2730454615",	-- Kobold Hoarder
+	"481692085",	-- Super Discobombulator
+	"959443907",	-- Detective Taser
+	"1473581448",	-- Death Faker
+	"1599710095",	-- Kamehameha
+	"1777819207",	-- Marker Role
 }
 
 for _, id in ipairs(list) do 
