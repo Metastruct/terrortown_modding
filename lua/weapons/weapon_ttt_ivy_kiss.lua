37911bf7ceb1b81e5326e1a1b78f8888297288a5
Ryan <ryan.rbsr@gmail.com>
2024-03-26 13:58:15 +0100

    add poison effects

--- lua/weapons/weapon_ttt_ivy_kiss.lua
+++ lua/weapons/weapon_ttt_ivy_kiss.lua
@@ -21,7 +21,6 @@ if CLIENT then
 	SWEP.Icon = "vgui/ttt/icon_ivy_kiss"
 end
 
-local all_timers = {}
 function SWEP:Kiss()
 	local victim = self:GetKissVictim()
 	if not IsValid(victim) then return end
@@ -36,15 +35,24 @@ function SWEP:Kiss()
 
 		local timer_name = ("ivy_poison_[%d]"):format(victim:EntIndex())
 		timer.Simple(5, function()
-			table.insert(all_timers, timer_name)
 			timer.Create(timer_name, 2, 0, function()
-				if not IsValid(victim) or not victim:Alive() then
+				if not IsValid(victim) then
+					timer.Remove(timer_name)
+					return
+				end
+
+				if not victim:Alive() and victim.SetDrunkFactor then
+					victim:SetDrunkFactor(0)
 					timer.Remove(timer_name)
 					return
 				end
 
 				victim:TakeDamageInfo(dmg_info)
 			end)
+
+			if IsValid(victim) and victim:Alive() and victim.SetDrunkFactor then
+				victim:SetDrunkFactor(100)
+			end
 		end)
 	end
 
@@ -53,13 +61,19 @@ end
 
 if SERVER then
 	hook.Add("TTTEndRound", "weapon_ttt_ivy_kiss", function()
-		for _, timer_name in ipairs(all_timers) do
-			timer.Remove(timer_name)
+		for _, ply in ipairs(player.GetAll()) do
+			local timer_name = ("ivy_poison_[%d]"):format(ply:EntIndex())
+			local timer_exists = timer.Exists(timer_name)
+			if timer_exists then
+				timer.Remove(timer_name)
+				ply:SetDrunkFactor(0)
+			end
 		end
 	end)
 
 	hook.Add("TTTPlayerUsedHealthStation", "weapon_ttt_ivy_kiss", function(ply)
 		local timer_name = ("ivy_poison_[%d]"):format(ply:EntIndex())
 		timer.Remove(timer_name)
+		ply:SetDrunkFactor(0)
 	end)
 end
