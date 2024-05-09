5a032aef3678f4a1f3f133fa774b0cbf9e68dcbe
Ryan <ryan.rbsr@gmail.com>
2024-05-09 18:29:04 +0200

    yeah

--- lua/chaos_rounds/supernova.lua
+++ lua/chaos_rounds/supernova.lua
@@ -3,22 +3,22 @@ ROUND.Name = "Super-nova"
 ROUND.Description = "Every innocent explodes on death!"
 
 function ROUND:Start()
-	if SERVER then
-		timer.Simple(1, function()
+	timer.Simple(1, function()
+		if SERVER then
 			for _, ply in pairs(player.GetAll()) do
 				if ply ~= ROLE_INNOCENT then continue end
 
 				ply:SetRole(ROLE_NOVA)
 			end
-		end)
-	end
-
-	if CLIENT then
-		local ply = LocalPlayer()
-		if ply:GetRole() == ROLE_INNOCENT then
-			ply:SetRole(ROLE_NOVA)
 		end
-	end
+
+		if CLIENT then
+			local ply = LocalPlayer()
+			if ply:GetRole() == ROLE_INNOCENT then
+				ply:SetRole(ROLE_NOVA)
+			end
+		end
+	end)
 end
 
 function ROUND:Finish()
