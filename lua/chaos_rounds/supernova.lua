5bf29690dae4b2da0eba7dfcb72717dd5227ffb4
Ryan <ryan.rbsr@gmail.com>
2024-05-09 18:47:03 +0200

    maybe fix?

--- lua/chaos_rounds/supernova.lua
+++ lua/chaos_rounds/supernova.lua
@@ -6,9 +6,9 @@ function ROUND:Start()
 	timer.Simple(1, function()
 		if SERVER then
 			for _, ply in pairs(player.GetAll()) do
-				if ply ~= ROLE_INNOCENT then continue end
-
-				ply:SetRole(ROLE_NOVA)
+				if ply:GetRole() == ROLE_INNOCENT then
+					ply:SetRole(ROLE_NOVA)
+				end
 			end
 		end
 
