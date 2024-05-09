b60a6008ebd8b4793b9418791a436eb9979f7381
Lixquid <lixquid@lixquid.com>
2024-05-09 03:14:13 +0100

    Fix role avoid menu

--- lua/terrortown/menus/gamemode/gameplay/cl_ttt_roleavoid.lua
+++ lua/terrortown/menus/gamemode/gameplay/cl_ttt_roleavoid.lua
@@ -4,22 +4,24 @@ CLGAMEMODESUBMENU.priority = 200
 CLGAMEMODESUBMENU.title = "submenu_roleavoid_title"
 CLGAMEMODESUBMENU.icon = Material("vgui/ttt/vskin/helpscreen/roles")
 
+local roleConvarsCreated = false
+
 ---@param parent Panel
 function CLGAMEMODESUBMENU:Populate(parent)
 	local form = vgui.CreateTTT2Form(parent, "header_roleavoid")
 
 	---@type {[1]: string, [2]: number}[]
-	local roles = {}
+	local roleList = {}
 	for _, role in next, roles.roleList do
-		roles[#roles + 1] = { role.name, role.index }
+		roleList[#roleList + 1] = { role.name, role.index }
 	end
 
 	-- Sort the roles by name
-	table.sort(roles, function(a, b)
+	table.sort(roleList, function(a, b)
 		return a[1] < b[1]
 	end)
 
-	for _, role in ipairs(roles) do
+	for _, role in ipairs(roleList) do
 		local roleName = role[1]
 		local roleIndex = role[2]
 
@@ -27,5 +29,11 @@ function CLGAMEMODESUBMENU:Populate(parent)
 			label = roleName,
 			serverConvar = "ttt2_avoidrole_" .. roleIndex,
 		})
+
+		if not roleConvarsCreated then
+			CreateClientConVar("ttt2_avoidrole_" .. roleIndex, "0", true, true)
+		end
 	end
+
+	roleConvarsCreated = true
 end
