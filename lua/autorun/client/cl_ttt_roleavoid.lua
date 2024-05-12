8a27e7b703ce81fa1d22c0c4e90256f56b2bfc47
Lixquid <lixquid@lixquid.com>
2024-05-12 20:16:42 +0100

    Change hook used to add role avoid convars

--- lua/autorun/client/cl_ttt_roleavoid.lua
+++ lua/autorun/client/cl_ttt_roleavoid.lua
@@ -1,4 +1,4 @@
-hook.Add("PlayerInitialSpawn", "ttt2_roleavoid", function(ply)
+hook.Add("TTTPrepareRound", "ttt2_roleavoid", function(ply)
 	for _, role in pairs(roles.roleList) do
 		if role.index ~= nil and role.index ~= roles.INNOCENT.index and not role.notSelectable then
 			CreateClientConVar("ttt2_avoidrole_" .. role.name, "0", true, true)
