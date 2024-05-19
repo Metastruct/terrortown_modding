27bed78b95ee8183a89efe90276c026ca4d0d228
Ryan <ryan.rbsr@gmail.com>
2024-05-19 12:58:49 +0200

    make the kill moves use +reload instead of +use

--- lua/autorun/bsmod_fix.lua
+++ lua/autorun/bsmod_fix.lua
@@ -1,6 +1,6 @@
-local Tag = "bsmod_integration"
-
+local TAG = "bsmod_integration"
 local convarMinHealth
+
 local function getConvarMinHealth()
 	convarMinHealth = convarMinHealth or GetConVar("bsmod_killmove_minhealth")
 	return convarMinHealth
@@ -8,13 +8,7 @@ end
 
 local function elligibleForKillMove(ply)
 	if GetRoundState and GetRoundState() ~= ROUND_ACTIVE then return false end
-	if not IsValid(ply)
-		or not ply:IsPlayer()
-		or (CLIENT and ply == LocalPlayer())
-		or not ply:Alive()
-	then
-		return false
-	end
+	if not IsValid(ply) or not ply:IsPlayer() or (CLIENT and ply == LocalPlayer()) or not ply:Alive() then return false end
 
 	local convar = getConvarMinHealth()
 	if convar and ply:Health() > convar:GetInt() then return false end
@@ -25,36 +19,53 @@ end
 if SERVER then
 	AddCSLuaFile()
 
-	hook.Add("InitPostEntity", Tag, function()
+	hook.Add("InitPostEntity", TAG, function()
 		local PLY = FindMetaTable("Player")
-
 		PLY.old_KillMove = PLY.old_KillMove or PLY.KillMove
 		if not PLY.old_KillMove then return end
 
 		function PLY:KillMove(...)
 			if GetRoundState and GetRoundState() ~= ROUND_ACTIVE then return end
 			if not self:IsTerror() then return end
-
 			PLY.old_KillMove(self, ...)
 		end
 	end)
 
-	hook.Add("PlayerUse", Tag, function(ply, ent)
-		if not _G.KMCheck then return end
-		if not elligibleForKillMove(ent) then return end
-
-		_G.KMCheck(ply)
+	local plys_pressing = {}
+	net.Receive(TAG, function(_, ply)
+		plys_pressing[ply] = true
+		timer.Create(TAG, 1, 1, function()
+			plys_pressing[ply] = nil
+		end)
 	end)
-else
-	local RED_COLOR = Color(255,0,0)
 
+	hook.Add("Think", TAG, function()
+		if not _G.KMCheck then return end
+		for ply, _ in pairs(plys_pressing) do
+			if not IsValid(ply) then
+				plys_pressing[ply] = nil
+				continue
+			end
+
+			local tr = ply:GetEyeTrace()
+			if elligibleForKillMove(tr.Entity) then
+				_G.KMCheck(ply)
+			end
+		end
+	end)
+end
+
+if CLIENT then
+	local RED_COLOR = Color(255, 0, 0)
 	local convarGlow
+
 	local function getConvarGlow()
 		convarGlow = convarGlow or GetConVar("bsmod_killmove_glow")
 		return convarGlow
 	end
 
-	hook.Add("TTTRenderEntityInfo", Tag, function(data)
+	local RELOAD_BIND = input.LookupBinding("+use", true) or "unknown"
+	hook.Add("TTTRenderEntityInfo", TAG, function(data)
 		local ent = data:GetEntity()
 		if not elligibleForKillMove(ent) then return end
 
@@ -69,7 +80,13 @@ else
 		data:EnableText()
 		data:EnableOutline()
 		data:SetOutlineColor(RED_COLOR)
+		data:AddDescriptionLine("Press [" .. RELOAD_BIND:upper() .. "] to FINISH them!")
+	end)
 
-		data:AddDescriptionLine("Press [" .. input.LookupBinding("+use", true):upper() .. "] to FINISH them!")
+	hook.Add("PlayerBindPress", TAG, function(_, bind)
+		if bind ~= "+reload" then return end
+
+		net.Start(TAG, true)
+		net.SendToServer()
 	end)
 end
