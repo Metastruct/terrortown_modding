61172ed4fbacc7926bb038471721c72da4bc001e
Ryan <ryan.rbsr@gmail.com>
2024-05-19 21:50:54 +0200

    REVERT BECAUSE NOBODY KNOWS WHAT THEY WANT :)

--- lua/autorun/bsmod_fix.lua
+++ lua/autorun/bsmod_fix.lua
@@ -1,26 +1,23 @@
-local TAG = "bsmod_integration"
-local convarMinHealth
+-- I SWEAR TO GOD IF ANYONE COMPLAINS THATS ITS ON E IM GONNA MURDER SOMEBODY - Earu
 
-local function getConvarMinHealth()
-	convarMinHealth = convarMinHealth or GetConVar("bsmod_killmove_minhealth")
-	return convarMinHealth
-end
-
+local Tag = "bsmod_integration"
 local function elligibleForKillMove(ply)
 	if GetRoundState and GetRoundState() ~= ROUND_ACTIVE then return false end
-	if not IsValid(ply) or not ply:IsPlayer() or (CLIENT and ply == LocalPlayer()) or not ply:Alive() then return false end
+	if not IsValid(ply) then return false end
+	if not ply:IsPlayer() then return false end
 
-	local convar = getConvarMinHealth()
-	if convar and ply:Health() > convar:GetInt() then return false end
+	if CLIENT and ply == LocalPlayer() then return false end
+
+	if not ply:Alive() then return false end
+	if ply:Health() > GetConVar("bsmod_killmove_minhealth"):GetInt() then return false end
 
 	return true
 end
 
 if SERVER then
 	AddCSLuaFile()
-	util.AddNetworkString(TAG)
 
-	hook.Add("InitPostEntity", TAG, function()
+	hook.Add("InitPostEntity", Tag, function()
 		local PLY = FindMetaTable("Player")
 		PLY.old_KillMove = PLY.old_KillMove or PLY.KillMove
 		if not PLY.old_KillMove then return end
@@ -28,42 +25,37 @@ if SERVER then
 		function PLY:KillMove(...)
 			if GetRoundState and GetRoundState() ~= ROUND_ACTIVE then return end
 			if not self:IsTerror() then return end
+
 			PLY.old_KillMove(self, ...)
 		end
+	end)
+
+	hook.Add("PlayerUse", Tag, function(ply, ent)
+		if not _G.KMCheck then return end
+		if not elligibleForKillMove(ent) then return end
+
+		_G.KMCheck(ply)
 	end)
 end
 
 if CLIENT then
-	local RED_COLOR = Color(255, 0, 0)
-	local convarGlow
-
-	local function getConvarGlow()
-		convarGlow = convarGlow or GetConVar("bsmod_killmove_glow")
-		return convarGlow
-	end
-
-	local RELOAD_BIND = input.LookupBinding("bsmod_killmove")
-	if not RELOAD_BIND then
-		RELOAD_BIND = "\'bsmod_killmove\' IS NOT BOUND"
-	else
-		RELOAD_BIND = RELOAD_BIND:upper()
-	end
-
-	hook.Add("TTTRenderEntityInfo", TAG, function(data)
+	local RED_COLOR = Color(255,0,0)
+	hook.Add("TTTRenderEntityInfo", Tag, function(data)
 		local ent = data:GetEntity()
 		if not elligibleForKillMove(ent) then return end
 
 		local dist = data:GetEntityDistance()
 		if dist > 200 then return end
 
-		local convar = getConvarGlow()
-		if convar then
-			convar:SetBool(false)
+		local glow_cvar = GetConVar("bsmod_killmove_glow")
+		if glow_cvar then
+			glow_cvar:SetBool(false)
 		end
 
 		data:EnableText()
 		data:EnableOutline()
+
 		data:SetOutlineColor(RED_COLOR)
-		data:AddDescriptionLine("Press [" .. RELOAD_BIND .. "] to FINISH them!")
+		data:AddDescriptionLine("Press [" .. input.LookupBinding("+use", true):upper() .. "] to FINISH them!")
 	end)
 end
