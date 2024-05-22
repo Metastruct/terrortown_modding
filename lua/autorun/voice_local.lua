e680c8928cd71f2012af204a589f8613169fbc75
Lixquid <lixquid@lixquid.com>
2024-05-23 00:52:27 +0100

    Add ability to set manual scale for 2d voice with SetVoiceVolumeScale

--- lua/autorun/voice_local.lua
+++ lua/autorun/voice_local.lua
@@ -1,8 +1,10 @@
 local tag = "TTTVoiceLocal"
 
-local cvarLocalVoiceEnable = CreateConVar("ttt_voice_local", "0", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED})
-local cvarLocalVoiceRangeMin = CreateConVar("ttt_voice_local_range_min", "200", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED})
-local cvarLocalVoiceRangeMax = CreateConVar("ttt_voice_local_range_max", "2500", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED})
+local cvarLocalVoiceEnable = CreateConVar("ttt_voice_local", "0", { FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED })
+local cvarLocalVoiceRangeMin = CreateConVar("ttt_voice_local_range_min", "200",
+	{ FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED })
+local cvarLocalVoiceRangeMax = CreateConVar("ttt_voice_local_range_max", "2500",
+	{ FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED })
 
 if SERVER then
 	local function CanHearVoiceChat(listener, speaker, teamVoice)
@@ -29,6 +31,23 @@ if SERVER then
 		hook.Add("TTT2CanHearVoiceChat", tag, CanHearVoiceChat)
 	end
 else
+	local voiceFactors = {}
+	-- HACK: Override SetVoiceVolumeScale to actually adjust an internal value
+	-- since we manually use SetVoiceVolumeScale every tick
+	local plyMetatable = FindMetaTable("Player")
+	if plyMetatable.SetVoiceVolumeScale_Original then
+		plyMetatable.SetVoiceVolumeScale = plyMetatable.SetVoiceVolumeScale_Original
+		plyMetatable.GetVoiceVolumeScale = plyMetatable.GetVoiceVolumeScale_Original
+	end
+	plyMetatable.SetVoiceVolumeScale_Original = plyMetatable.SetVoiceVolumeScale
+	plyMetatable.GetVoiceVolumeScale_Original = plyMetatable.GetVoiceVolumeScale
+	plyMetatable.SetVoiceVolumeScale = function(self, scale)
+		voiceFactors[self] = math.Clamp(scale, 0, 1)
+	end
+	plyMetatable.GetVoiceVolumeScale = function(self)
+		return voiceFactors[self] or 1
+	end
+
 	local inCirc = math.ease.InCirc
 
 	local globalVoiceSuffix = "_gvoice"
@@ -37,28 +56,33 @@ else
 	local selfRoleDisabledTeamVoiceRecv, minDist, maxDist
 
 	local function IsRoleChatting(pl)
-	    local plTeam = pl:GetTeam()
-	    local plRoleData = pl:GetSubRoleData()
+		local plTeam = pl:GetTeam()
+		local plRoleData = pl:GetSubRoleData()
 
-	    selfRoleDisabledTeamVoiceRecv = selfRoleDisabledTeamVoiceRecv or LocalPlayer():GetSubRoleData().disabledTeamVoiceRecv
+		selfRoleDisabledTeamVoiceRecv = selfRoleDisabledTeamVoiceRecv or
+			LocalPlayer():GetSubRoleData().disabledTeamVoiceRecv
 
-	    return not plRoleData.unknownTeam
-	        and not plRoleData.disabledTeamVoice
-	        and not selfRoleDisabledTeamVoiceRecv
-	        and plTeam != TEAM_NONE
-	        and not TEAMS[plTeam].alone
-	        and not pl[plTeam .. globalVoiceSuffix]
+		return not plRoleData.unknownTeam
+			and not plRoleData.disabledTeamVoice
+			and not selfRoleDisabledTeamVoiceRecv
+			and plTeam ~= TEAM_NONE
+			and not TEAMS[plTeam].alone
+			and not pl[plTeam .. globalVoiceSuffix]
 	end
 
-	local function VoiceScale(localPos, speakerPos)
+	---@param localPos Vector
+	---@param speakerPos Vector
+	---@param speaker Player
+	local function VoiceScale(localPos, speakerPos, speaker)
 		local dist = speakerPos:Distance(localPos) - minDist
 		local scale = 1 - (dist / (maxDist - minDist))
+		local extraFactor = voiceFactors[speaker] or 1
 
-		return inCirc(scale > 1 and 1 or (scale < 0 and 0 or scale))
+		return inCirc(math.Clamp(scale * extraFactor, 0, 1))
 	end
 
 	hook.Add("PlayerStartVoice", tag, function(pl)
-		if pl != LocalPlayer() then
+		if pl ~= LocalPlayer() then
 			pl.VoiceChatting = true
 		end
 	end)
@@ -70,7 +94,7 @@ else
 	hook.Add("Tick", tag, function()
 		if not cvarLocalVoiceEnable:GetBool() then
 			if resetIfDisabled then
-				for k,v in ipairs(player.GetAll()) do
+				for k, v in ipairs(player.GetAll()) do
 					v:SetVoiceVolumeScale(1)
 				end
 
@@ -86,14 +110,14 @@ else
 		if not IsValid(pl) then return end
 
 		local now = RealTime()
-		local roundNotActive = GetRoundState() != ROUND_ACTIVE
+		local roundNotActive = GetRoundState() ~= ROUND_ACTIVE
 
 		minDist, maxDist = cvarLocalVoiceRangeMin:GetInt(), cvarLocalVoiceRangeMax:GetInt()
 
 		local plPos = pl:GetPos()
 
-		for k,v in ipairs(player.GetAll()) do
-			if v.VoiceChatting or (pl != v and now >= (v.VoiceChatNextTick or 0)) then
+		for k, v in ipairs(player.GetAll()) do
+			if v.VoiceChatting or (pl ~= v and now >= (v.VoiceChatNextTick or 0)) then
 				local scale = (roundNotActive or not v:IsTerror() or IsRoleChatting(v))
 					and 1
 					or VoiceScale(plPos, v:GetPos())
