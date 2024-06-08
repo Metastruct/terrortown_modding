efea6a5169d62ce2f781c0d55c571336b5fecc4c
TW1STaL1CKY <tw1stal1cky@gmail.com>
2024-06-08 01:34:29 +0100

    Regular Knife and Shank Knife tweaks
    - Regular: Increased damage, faster attack speed, slightly faster deploy speed
    - Shank: Slightly faster attack speed and deploy speed

--- lua/autorun/entity_tweaks.lua
+++ lua/autorun/entity_tweaks.lua
@@ -12,47 +12,69 @@ util.OnInitialize(function()
 		ENT.dropAngleThreshold = 0.925
 	end
 
-	-- Jihad Bomb: Set explosion radius and disable damage behind walls
-	-- PS: this is terrible unfortunately the developer of this weapon left no configuration possible...
-	ENT = weapons.GetStored("weapon_ttt_jihad_bomb")
+	-- Regular Knife: Increase damage and attack speed, remove the weird artifical attack delay on equip (DeploySpeed handles this anyway)
+	ENT = weapons.GetStored("weapon_ttt_knife")
 	if ENT then
-		function ENT:Explode()
-			local pos = self:GetPos()
-			local dmg = 200
-			local dmgowner = self:GetOwner()
+		ENT.Primary.Damage = 75
+		ENT.Primary.Delay = 0.75
 
-			local r_inner = 250
-			local r_outer = r_inner * 1.15
+		if SERVER then
+			function ENT:Equip()
+				if self:IsOnFire() then
+					self:Extinguish()
+				end
 
-			self:EmitSound("weapons/jihad_bomb/big_explosion.wav", 400, math.random(100, 125))
+				if self:HasSpawnFlags(SF_WEAPON_START_CONSTRAINED) then
+					local flags = self:GetSpawnFlags()
+					local newflags = bit.band(self:GetSpawnFlags(), bit.bnot(SF_WEAPON_START_CONSTRAINED))
 
-			-- change body to a random charred body
-			local model = "models/humans/charple0" .. math.random(1, 4) .. ".mdl"
-			self:GetOwner():SetModel(model)
-
-			-- explosion damage
-			util.BlastDamage(self, dmgowner, pos, r_outer, dmg)
-
-			local effect = EffectData()
-			effect:SetStart(pos)
-			effect:SetOrigin(pos)
-			effect:SetScale(r_outer)
-			effect:SetRadius(r_outer)
-			effect:SetMagnitude(dmg)
-			util.Effect("Explosion", effect, true, true)
-
-			-- make sure the owner dies anyway
-			if (SERVER and IsValid(dmgowner) and dmgowner:Alive()) then
-				dmgowner:Kill()
+					self:SetKeyValue("spawnflags", newflags)
+				end
 			end
-
-			--BurnOwnersBody(model)
-			self:Remove()
 		end
 	end
 
 	if SERVER then
 		-- Serverside only tweaks
+
+		-- Jihad Bomb: Set explosion radius and disable damage behind walls
+		-- PS: this is terrible unfortunately the developer of this weapon left no configuration possible...
+		ENT = weapons.GetStored("weapon_ttt_jihad_bomb")
+		if ENT then
+			function ENT:Explode()
+				local pos = self:GetPos()
+				local dmg = 200
+				local dmgowner = self:GetOwner()
+
+				local r_inner = 250
+				local r_outer = r_inner * 1.15
+
+				self:EmitSound("weapons/jihad_bomb/big_explosion.wav", 400, math.random(100, 125))
+
+				-- change body to a random charred body
+				local model = "models/humans/charple0" .. math.random(1, 4) .. ".mdl"
+				self:GetOwner():SetModel(model)
+
+				-- explosion damage
+				util.BlastDamage(self, dmgowner, pos, r_outer, dmg)
+
+				local effect = EffectData()
+				effect:SetStart(pos)
+				effect:SetOrigin(pos)
+				effect:SetScale(r_outer)
+				effect:SetRadius(r_outer)
+				effect:SetMagnitude(dmg)
+				util.Effect("Explosion", effect, true, true)
+
+				-- make sure the owner dies anyway
+				if (SERVER and IsValid(dmgowner) and dmgowner:Alive()) then
+					dmgowner:Kill()
+				end
+
+				--BurnOwnersBody(model)
+				self:Remove()
+			end
+		end
 
 		-- All grenade projectiles: Disable air-drag for slightly better flight, following trajectory line more accurately
 		local grenadeProjectileBaseClass = "ttt_basegrenade_proj"
