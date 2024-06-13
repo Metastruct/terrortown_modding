e42f60519889b83f4f75840594a423a9ba724c57
Ryan <ryan.rbsr@gmail.com>
2024-06-13 17:52:36 +0200

    very simple chaos round but funny

---
+++ lua/chaos_rounds/vengeful_spirits.lua
@@ -0,0 +1,15 @@
+local ROUND = {}
+ROUND.Name = "Vengeful Spirits"
+ROUND.Description = "Dead people can possess the living."
+
+if SERVER then
+	function ROUND:Start()
+		for _, ply in pairs(player.GetAll()) do
+			if not ply:IsTerror() then continue end
+
+			ply:GiveEquipmentItem("item_demonic_possession")
+		end
+	end
+end
+
+return RegisterChaosRound(ROUND)
