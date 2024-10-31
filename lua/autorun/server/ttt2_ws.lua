757fc90890b519a62ebd2922b79d856d57f965c7
TW1STaL1CKY <tw1stal1cky@gmail.com>
2024-10-31 19:59:59 +0000

    baker sucks lol

--- lua/autorun/server/ttt2_ws.lua
+++ lua/autorun/server/ttt2_ws.lua
@@ -28,7 +28,6 @@ local addons = {
 	"3003509443",	-- Detective Toy Car (Cozy Coupe)
 
 	-- We may not keep these but testing for Halloween
-	"3298985434",	-- Baker & Famine Role
 	"2646495752",	-- Flashcut
 }
 
