799247da815da711605f8e6bbc87d4e168666a3c
TW1STaL1CKY <tw1stal1cky@gmail.com>
2024-12-30 00:26:57 +0000

    Lag Grenade fixes
    - Fixed Lag Grenade displacing spectators and people in vehicles
    - Code optimisations

--- lua/entities/ttt_laggrenade_proj.lua
+++ lua/entities/ttt_laggrenade_proj.lua
@@ -22,8 +22,10 @@ local droneSound = Sound("ambient/levels/citadel/portal_beam_loop1.wav")
 
 function ENT:Initialize()
     self:SetColor(Color(255, 0, 0, 255))
+
     ---@type {[Player]: Vector}
     self.PlayerPositions = {}
+
     if BaseClass then
         return BaseClass.Initialize(self)
     end
@@ -32,6 +34,7 @@ end
 function ENT:Explode(tr)
     if CLIENT or self.Exploded then return end
     self.Exploded = true
+
     local pos = self:GetPos()
 
     self:SetNoDraw(true)
@@ -43,42 +46,51 @@ function ENT:Explode(tr)
     -- Effects
     self:EmitSound(droneSound, 75 * cvarRadius:GetFloat() / 400, 200)
     sound.Play(explodeSound, pos, 100, 100)
+
     local effectData = EffectData()
     effectData:SetStart(pos)
     effectData:SetOrigin(pos)
+
     util.Effect("Explosion", effectData, true, true)
     util.Effect("cball_explode", effectData, true, true)
+
     self.NextJumpTick = CurTime()
 
     timer.Simple(cvarDuration:GetFloat(), function()
         if IsValid(self) then
             self:StopSound(droneSound)
         end
+
         SafeRemoveEntity(self)
     end)
 end
 
-function ENT:Think()
-    self.BaseClass.Think(self)
-    if self.NextJumpTick and self.NextJumpTick < CurTime() then
-        for _, ply in ipairs(player.GetAll()) do
-            local pos = ply:GetPos()
-            if pos:Distance(self:GetPos()) < cvarRadius:GetFloat() then
-                local savedPos = self.PlayerPositions[ply]
-                if not savedPos or math.random() < 0.2 then
-                    self.PlayerPositions[ply] = pos
-                else
-                    ply:SetPos(savedPos)
-                end
-            end
-        end
-        self.NextJumpTick = CurTime() + math.random() * 0.6 + 0.2
-    end
-    if CLIENT then return end
-end
-
 if SERVER then
     util.AddNetworkString("ttt_laggrenade_detonation")
+
+    function ENT:Think()
+        self.BaseClass.Think(self)
+
+        if self.NextJumpTick and self.NextJumpTick < CurTime() then
+            local radiusSqr = cvarRadius:GetFloat() ^ 2
+
+            for _, ply in ipairs(player.GetAll()) do
+                local pos = ply:GetPos()
+
+                if ply:IsTerror() and not ply:InVehicle() and pos:DistToSqr(self:GetPos()) < radiusSqr then
+                    local savedPos = self.PlayerPositions[ply]
+
+                    if not savedPos or math.random() < 0.2 then
+                        self.PlayerPositions[ply] = pos
+                    else
+                        ply:SetPos(savedPos)
+                    end
+                end
+            end
+
+            self.NextJumpTick = CurTime() + math.random() * 0.6 + 0.2
+        end
+    end
 else
     ---@type {[number]: {pos: Vector, expires: number, nextparticle: number}}
     local activeGrenades = {}
@@ -92,15 +104,18 @@ else
 
     hook.Add("RenderScreenspaceEffects", "ttt_laggrenade", function()
         local pos = LocalPlayer():GetPos()
-        local fps = cvarFPS:GetFloat()
-        local radius = cvarRadius:GetFloat()
         local renderingLag = false
 
-        for id, detData in pairs(activeGrenades) do
+        local fps = cvarFPS:GetFloat()
+        local radius = cvarRadius:GetFloat()
+        local radiusSqr = radius ^ 2
+
+        for id, detData in ipairs(activeGrenades) do
             if detData.expires < CurTime() then
                 table.remove(activeGrenades, id)
-            elseif not renderingLag and pos:Distance(detData.pos) < cvarRadius:GetFloat() then
+            elseif not renderingLag and pos:DistToSqr(detData.pos) < radiusSqr then
                 renderingLag = true
+
                 DrawMotionBlur(1, 1, 1 / fps)
 
                 -- Draw a "low signal" icon
@@ -111,24 +126,25 @@ else
                 surface.DrawRect(x + 1, y + 61, 20, 30)
                 surface.DrawOutlinedRect(x + 31, y + 31, 20, 60)
                 surface.DrawOutlinedRect(x + 61, y + 1, 20, 90)
+
                 surface.SetDrawColor(255, 0, 0, 255)
                 surface.DrawRect(x, y + 60, 20, 30)
                 surface.DrawOutlinedRect(x + 30, y + 30, 20, 60)
                 surface.DrawOutlinedRect(x + 60, y, 20, 90)
             end
+
             if detData.nextparticle < CurTime() then
                 detData.nextparticle = CurTime() + 0.08
 
                 local e = EffectData()
-                e:SetOrigin(detData.pos + Vector(
-                    math.random(-radius, radius),
-                    math.random(-radius, radius),
-                    math.random(-radius, radius)
-                ))
-                e:SetNormal(Vector(0, 0, 1))
+
+                e:SetOrigin(detData.pos + VectorRand(-radius, radius))
+                e:SetNormal(vector_up)
                 e:SetMagnitude(1)
                 e:SetRadius(16)
+
                 util.Effect("Sparks", e, true, true)
+
                 if math.random(1, 3) == 1 then
                     util.Effect("cball_explode", e, true, true)
                 end
