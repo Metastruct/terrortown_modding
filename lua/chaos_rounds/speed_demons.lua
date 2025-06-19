2fcf809e9e9799ad094461d583beb2ef0712df04
Ryan <ryan.rbsr@gmail.com>
2025-06-19 20:34:03 +0200

    improvements to speed demons

--- lua/chaos_rounds/speed_demons.lua
+++ lua/chaos_rounds/speed_demons.lua
@@ -11,16 +11,19 @@ local DAMAGE_MULTIPLIER = 0.7
 if SERVER then
 	function ROUND:Start()
 		-- Increase player speeds
+
+		self.BaseWalkSpeed = Entity(1):GetWalkSpeed()
+		self.BaseRunSpeed = Entity(1):GetRunSpeed()
+		self.BaseLadderClimbSpeed = Entity(1):GetLadderClimbSpeed()
+		self.BaseJumpPower = Entity(1):GetJumpPower()
+
 		for _, ply in ipairs(player.GetAll()) do
 			if not ply:IsTerror() then continue end
 
-			local baseWalk = ply:GetWalkSpeed()
-			local baseRun = ply:GetRunSpeed()
-
-			ply:SetWalkSpeed(baseWalk * SPEED_MULTIPLIER)
-			ply:SetRunSpeed(baseRun * SPEED_MULTIPLIER)
-			ply:SetLadderClimbSpeed(baseRun * SPEED_MULTIPLIER)
-			ply:SetJumpPower(200 * SPEED_MULTIPLIER)
+			ply:SetWalkSpeed(self.BaseWalkSpeed * SPEED_MULTIPLIER)
+			ply:SetRunSpeed(self.BaseRunSpeed * SPEED_MULTIPLIER)
+			ply:SetLadderClimbSpeed(self.BaseLadderClimbSpeed * SPEED_MULTIPLIER)
+			ply:SetJumpPower(self.BaseJumpPower * SPEED_MULTIPLIER)
 		end
 
 		-- Modify weapon fire rates and damage
@@ -32,23 +35,20 @@ if SERVER then
 
 		-- Handle player respawns/revives to reapply speed benefits
 		hook.Add("PlayerSpawn", TAG, function(ply)
-			local baseWalk = ply:GetWalkSpeed()
-			local baseRun = ply:GetRunSpeed()
-
-			ply:SetWalkSpeed(baseWalk * SPEED_MULTIPLIER)
-			ply:SetRunSpeed(baseRun * SPEED_MULTIPLIER)
-			ply:SetLadderClimbSpeed(baseRun * SPEED_MULTIPLIER)
-			ply:SetJumpPower(200 * SPEED_MULTIPLIER)
+			ply:SetWalkSpeed(self.BaseWalkSpeed * SPEED_MULTIPLIER)
+			ply:SetRunSpeed(self.BaseRunSpeed * SPEED_MULTIPLIER)
+			ply:SetLadderClimbSpeed(self.BaseLadderClimbSpeed * SPEED_MULTIPLIER)
+			ply:SetJumpPower(self.BaseJumpPower * SPEED_MULTIPLIER)
 		end)
 	end
 
 	function ROUND:Finish()
 		-- Reset player speeds
 		for _, ply in ipairs(player.GetAll()) do
-			ply:SetWalkSpeed(220) -- Default TTT walk speed
-			ply:SetRunSpeed(220) -- Default TTT run speed
-			ply:SetLadderClimbSpeed(220)
-			ply:SetJumpPower(200)
+			ply:SetWalkSpeed(self.BaseWalkSpeed)
+			ply:SetRunSpeed(self.BaseRunSpeed)
+			ply:SetLadderClimbSpeed(self.BaseLadderClimbSpeed)
+			ply:SetJumpPower(self.BaseJumpPower)
 		end
 
 		hook.Remove("EntityTakeDamage", TAG)
@@ -60,8 +60,8 @@ if CLIENT then
 	function ROUND:Start()
 		-- Add motion blur effect
 		hook.Add("RenderScreenspaceEffects", TAG, function()
-			if LocalPlayer():GetVelocity():Length() > 100 then
-				DrawMotionBlur(0.2, 0.8, 0.01)
+			if LocalPlayer():GetVelocity():Length() > 200 then
+				DrawMotionBlur(0.2, 0.25, 0.01)
 			end
 		end)
 	end
