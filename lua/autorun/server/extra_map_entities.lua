fc6c744b113af0760e7fa2bcffbaeb305c5863d8
TW1STaL1CKY <tw1stal1cky@gmail.com>
2025-06-29 18:32:34 +0100

    Added system for spawning extra map entities
    This allows custom placed entities (like props) to be automatically spawned each round. These ents can spawn every round or by chance.

---
+++ lua/autorun/server/extra_map_entities.lua
@@ -0,0 +1,195 @@
+local dataFolderPath = "ttt2_extra_mapents/"
+
+local TAB = {}
+
+local function RollChance(chance)
+	return not isnumber(chance) and true or math.random() <= chance
+end
+
+function TAB.GetMapFilePath()
+	return dataFolderPath .. game.GetMap() .. ".dat"
+end
+
+--[[ DATA FORMAT:
+{
+	id = NUMBER -- group ID
+	chance = NUMBER -- decimal chance for this group to spawn: 0-1 (only if group ID is not 0)
+	ents = {
+		class = STRING -- classname of the entity to spawn
+		pos = VECTOR -- position
+		ang = ANGLE -- angle
+		model = STRING -- model path
+		scale = NUMBER -- model scale
+		frozen = BOOL -- should freeze prop?
+		chance = NUMBER -- decimal chance for this entity to spawn: 0-1 (only if group ID is 0)
+	}
+} ]]
+
+function TAB.LoadDataAsync(func)
+	file.AsyncRead(TAB.GetMapFilePath(), "DATA", function(fileName, gamePath, status, data)
+		if status == FSASYNC_OK then
+			TAB.MapData = util.JSONToTable(data)
+
+			MsgN("[ExtraMapEnts] Loaded map data successfully")
+		else
+			MsgN("[ExtraMapEnts] Failed to load map data, FSASYNC status code ", status)
+		end
+
+		if isfunction(func) then
+			func(fileName, gamePath, status, data)
+		end
+	end)
+end
+
+function TAB.SaveData()
+	local mapData = TAB.MapData
+
+	if istable(mapData) then
+		local saveTable = {}
+
+		-- Iterate through the map data table to ensure it is clean before saving it
+		for _, v in pairs(mapData) do
+			if not istable(v)
+				or not istable(v.ents)
+				or table.Count(v.ents) == 0
+			then continue end
+
+			local newTab = {
+				id = isnumber(v.id) and v.id or 0,
+				chance = isnumber(v.chance) and v.chance or nil,
+				ents = {}
+			}
+
+			for _, x in pairs(v.ents) do
+				if not isstring(x.class) or not isvector(x.pos) then continue end
+
+				newTab.ents[#newTab.ents + 1] = {
+					class = x.class,
+					pos = x.pos,
+					ang = isangle(x.ang) and x.ang or nil,
+					model = isstring(x.model) and x.model or nil,
+					scale = isnumber(x.scale) and x.scale != 1 and x.scale or nil,
+					frozen = x.frozen == true or nil,
+					chance = isnumber(x.chance) and x.chance or nil
+				}
+			end
+
+			if #newTab.ents > 0 then
+				saveTable[#saveTable + 1] = newTab
+			end
+		end
+
+		local json = util.TableToJSON(saveTable)
+
+		file.CreateDir(string.Trim(dataFolderPath, "/"))
+		file.Write(TAB.GetMapFilePath(), json)
+
+		MsgN("[ExtraMapEnts] Map data saved successfully")
+		return true
+	end
+
+	MsgN("[ExtraMapEnts] Failed to save map data, no TTT2ExtraMapEnts.MapData table found")
+	return false
+end
+
+function TAB.AddEntity(ent, model, frozen, groupid, chance)
+	if not IsValid(ent) then return false end
+
+	if not istable(TAB.MapData) then
+		TAB.MapData = {}
+	end
+
+	local id = isnumber(groupid) and groupid or 0
+
+	local class = ent:GetClass()
+	local scale = ent:GetModelScale()
+
+	local newTab = {
+		class = class,
+		pos = ent:GetPos(),
+		ang = ent:GetAngles(),
+		model = model or (string.StartsWith(class, "prop_") and ent:GetModel() or nil),
+		scale = scale != 1 and scale or nil,
+		frozen = frozen,
+		chance = id == 0 and isnumber(chance) and chance or nil
+	}
+
+	for _, v in pairs(TAB.MapData) do
+		if v.id == id then
+			if istable(v.ents) then
+				v.ents[#v.ents + 1] = newTab
+			else
+				v.ents = { newTab }
+			end
+
+			return true
+		end
+	end
+
+	TAB.MapData[#TAB.MapData + 1] = {
+		id = id,
+		chance = id != 0 and isnumber(chance) and chance or nil,
+		ents = { newTab }
+	}
+
+	return true
+end
+
+function TAB.SpawnEntities(forceSpawn)
+	local mapData = TAB.MapData
+	if not istable(mapData) then return end
+
+	for _, v in pairs(mapData) do
+		if not istable(v)
+			or not istable(v.ents)
+			or table.Count(v.ents) == 0
+		then continue end
+
+		local isGroupZero = v.id == 0
+
+		if forceSpawn or (isGroupZero or RollChance(v.chance)) then
+			for _, x in pairs(v.ents) do
+				if not isstring(x.class)
+					or not isvector(x.pos)
+					or (not forceSpawn and isGroupZero and not RollChance(x.chance))
+				then continue end
+
+				local ent = ents.Create(x.class)
+
+				ent:SetPos(x.pos)
+
+				if isangle(x.ang) then ent:SetAngles(x.ang) end
+				if isstring(x.model) then ent:SetModel(x.model) end
+
+				ent:Spawn()
+
+				if isnumber(x.scale) then
+					ent:SetModelScale(x.scale, 0.00001)
+
+					timer.Simple(0.03, function()
+						if IsValid(ent) then
+							ent:Activate()
+						end
+					end)
+				end
+
+				if x.frozen then
+					local phys = ent:GetPhysicsObject()
+					if IsValid(phys) then
+						phys:EnableMotion(false)
+					end
+
+					ent:SetMoveType(MOVETYPE_NONE)
+				end
+			end
+		end
+	end
+end
+
+hook.Add("TTTPrepareRound", "TTT2ExtraMapEnts", function()
+	TAB.SpawnEntities()
+end)
+
+TAB.LoadDataAsync()
+
+TTT2ExtraMapEnts = TAB
