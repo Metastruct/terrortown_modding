d7edeec4ce33d7bfab0800fe580989edb83af549
TW1STaL1CKY <tw1stal1cky@gmail.com>
2025-10-13 01:00:38 +0100

    Rewrote the role preference system
    - Instead of rerolling everyone's roles, the system now only edits roles that need changing
    - Should work better in general
    - Compatible with TTT2's role de-randomisation
    - No longer doxes people's roles in admin console :troll:

--- lua/autorun/server/sv_ttt_rolepreference.lua
+++ lua/autorun/server/sv_ttt_rolepreference.lua
@@ -1,93 +1,126 @@
----@param ply Player
----@param roleName string
----@return number preference Between 0 and 1, inclusive
-local function getPlayerRolePreference(ply, roleName)
+local function getPlayerRolePreference(pl, roleName)
 	-- Bots always return 0 for GetInfoNum, no matter what the default value is
-	if ply:IsBot() then return 1 end
-	return math.Clamp(ply:GetInfoNum("ttt2_rolepreference_" .. roleName, 1), 0, 1)
+	if pl:IsBot() then return 1 end
+
+	return math.Clamp(pl:GetInfoNum("ttt2_rolepreference_" .. roleName, 1), 0, 1)
 end
 
----@param roleMap {[Player]: number}
 local function ReallocateRoles(roleMap)
-	local NO_ROLE_ASSIGNED = -1
+	local innocentId = ROLE_INNOCENT
 
-	-- Get all the roles that are in-play for this round, and a list of players
-	---@type Player[]
-	local players = {}
-	---@type number[]
-	local availableRoles = {}
-	for ply, v in pairs(roleMap) do
-		if v ~= roles.INNOCENT.id then
-			availableRoles[#availableRoles + 1] = v
+	local innocentInfo = {}
+	local rolesToPassOn = {}
+
+	for pl, roleId in pairs(roleMap) do
+		if roleId == innocentId then
+			innocentInfo[#innocentInfo + 1] = {
+				pl = pl,
+				weights = pl:GetRoleWeightTable()
+			}
+
+			continue
 		end
-		players[#players + 1] = ply
-		roleMap[ply] = NO_ROLE_ASSIGNED
+
+		local role = roles.GetByIndex(roleId)
+
+		local pref = getPlayerRolePreference(pl, role.name)
+		if pref >= 1 then continue end
+
+		local shouldSwapRole = pref <= 0 or math.random() <= pref
+		if not shouldSwapRole then continue end
+
+		-- Make them an innocent, add them to the innocents pool, and add the role to the list to pass on
+		roleMap[pl] = innocentId
+
+		innocentInfo[#innocentInfo + 1] = {
+			pl = pl,
+			weights = pl:GetRoleWeightTable()
+		}
+
+		rolesToPassOn[#rolesToPassOn + 1] = {
+			id = role.index,
+			name = role.name,
+			originalOwner = pl
+		}
 	end
 
-	-- Assign roles to players
-	for _, role in pairs(availableRoles) do
-		local roleName = roles.GetByIndex(role).name
-		local forced = false
+	if #rolesToPassOn > 0 then
+		local minWeight = roleselection.cv.ttt_role_derandomize_min_weight:GetInt()
 
-		-- Get a list of players and the cumulative preference for each player
-		local preferenceSum = 0
-		---@type {[1]: Player, [2]: number}[]
-		local plyPreferences = {}
-		for _, ply in pairs(players) do
-			local preference = getPlayerRolePreference(ply, roleName)
-			if preference > 0 and roleMap[ply] == NO_ROLE_ASSIGNED then
-				preferenceSum = preferenceSum + preference
-				plyPreferences[#plyPreferences + 1] = { ply, preference }
-			end
-		end
+		local rolesToForce = {}
 
-		---@type Player
-		local ply = nil
+		for i = 1, #rolesToPassOn do
+			local roleInfo = rolesToPassOn[i]
 
-		if preferenceSum == 0 then
-			-- If there are no players that can be assigned this role, consider
-			-- everyone and send a message to them
-			forced = true
-			ply = players[math.random(#players)]
-		else
-			-- Assign the role to a random player
-			local choice = math.random() * preferenceSum
-			for _, plyPreference in pairs(plyPreferences) do
-				local potentialPly = plyPreference[1]
-				local preference = plyPreference[2]
-				if choice <= preference then
-					ply = potentialPly
-					break
-				else
-					choice = choice - preference
+			local totalWeight = 0
+			local availables = {}
+
+			for x = 1, #innocentInfo do
+				local innoInfo = innocentInfo[x]
+
+				-- Don't attempt to give the role back to who originally had it
+				if roleInfo.originalOwner == innoInfo.pl then continue end
+
+				local pref = getPlayerRolePreference(innoInfo.pl, roleInfo.name)
+				if pref > 0 then
+					local weight = (innoInfo.weights[roleInfo.index] or minWeight) * pref
+
+					totalWeight = totalWeight + weight
+					availables[#availables + 1] = {
+						pl = pl,
+						weight = weight
+					}
 				end
 			end
-			if ply == nil then
-				ErrorNoHalt("Failed to assign role " .. roleName .. " to a player ERROR")
-				ply = plyPreferences[#plyPreferences][1]
+
+			if #availables > 0 then
+				local chosenValue = math.random() * totalWeight
+				local progressWeight = 0
+
+				for x = 1, #availables do
+					local avInfo = availables[x]
+
+					progressWeight = progressWeight + avInfo.weight
+
+					if progressWeight <= chosenValue then
+						-- Pass the role onto this player using weighted chance (based on weight * preference scale)
+						roleMap[avInfo.pl] = roleInfo.id
+
+						-- Take this player out the innocents pool
+						for y = 1, #innocentInfo do
+							if innocentInfo[y].pl == avInfo.pl then
+								table.remove(innocentInfo, y)
+								break
+							end
+						end
+
+						print(string.format("[RolePreference] Passed '%s' role to another player", roleInfo.name))
+
+						break
+					end
+				end
+			else
+				rolesToForce[#rolesToForce + 1] = roleInfo
 			end
 		end
 
-		print("Giving role " ..
-			roleName ..
-			" to " .. ply:Nick() .. " (forced: " .. tostring(forced) .. ")")
-		roleMap[ply] = role
+		for i = 1, #rolesToForce do
+			local roleInfo = rolesToForce[i]
 
-		-- Send a message to the player if they were forced into the role
-		if forced then
-			ply:ChatPrint("Sorry; there were no players that could be assigned the role " ..
-				roleName .. ", so you were forced into it.")
+			local chosenId = math.random(#innocentInfo)
+			local pl = innocentInfo[chosenId].pl
+
+			-- Force the orphan role on this player, let them know it was forced, and take them out the innocents pool
+			roleMap[pl] = roleInfo.id
+
+			table.remove(innocentInfo, chosenId)
+
+			pl:ChatPrint("Due to no-one wanting to take this role, you've been randomly chosen to take it. Sorry!")
+			print(string.format("[RolePreference] FORCED '%s' role to another player", roleInfo.name))
 		end
 	end
 
-	-- Everyone that wasn't assigned a role is now an innocent
-	for _, ply in pairs(players) do
-		if roleMap[ply] == NO_ROLE_ASSIGNED then
-			roleMap[ply] = roles.INNOCENT.id
-		end
-	end
-
-	-- we want to use the meta version not to break anything
+	-- We want to use the meta version not to break anything
 	hook.Run("TTT2MetaModifyFinalRoles", roleMap)
 end
 
